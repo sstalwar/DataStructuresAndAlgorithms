b4342e2d303df97d3d99854cc0e3a778e472321f
Sagar Talwar - Intern <Sagar.Talwar@bentley.com>
2020-07-12 11:44:35 -0400

    Initial commit with Selection Sort

---
+++ Sorting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorting Algorithms ");
+            int[] A = { 2, 7, 4, 1, 5, 3 };
+            Console.WriteLine("Actual Array: ");
+            A.ToList().ForEach(i => Console.Write(i.ToString() + " "));
+            Console.WriteLine();
+
+            //Selection Sort
+            var selection = new SelectionSort();
+            var sortedA = selection.Sort(A);
+            Console.WriteLine("\nSelection sorted Array: ");
+            sortedA.ToList().ForEach(i => Console.Write(i.ToString() + " "));
+
+            //Bubble Sort
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sorting/SelectionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting
+{
+    class SelectionSort
+    {
+        public int[] Sort(int[] A)
+        {
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    if (A[j] < A[min])
+                        min = j;
+                }
+                int temp = A[i];
+                A[i] = A[min];
+                A[min] = temp;
+            }
+            return A;
+        }
+    }
+}
