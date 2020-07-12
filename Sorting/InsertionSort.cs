fff48d75fbe6fb43b96914cc621476f7cd8a51a4
Sagar Talwar - Intern <Sagar.Talwar@bentley.com>
2020-07-12 19:41:30 -0400

    Added Insertion sort

---
+++ Sorting/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting
+{
+    class InsertionSort
+    {
+        public int[] Sort(int[] A)
+        {
+            for (int i = 1; i < A.Length; i++)
+            {
+                int temp = A[i];
+                int j = i - 1;
+                for (; j >= 0; j--)
+                {
+                    if (A[j] > temp)
+                    {
+                        A[j+1] = A[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                A[j+1] = temp;
+            }
+            return A;
+        }
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -8,23 +8,35 @@ namespace Sorting
         static void Main(string[] args)
         {
             Console.WriteLine("Sorting Algorithms ");
+
+
+            //Selection Sort
             int[] A = { 2, 7, 4, 1, 5, 3 };
             Console.WriteLine("Actual Array: ");
             A.ToList().ForEach(i => Console.Write(i.ToString() + " "));
-            Console.WriteLine();
-
-            //Selection Sort
+            //Console.WriteLine();
             var selection = new SelectionSort();
             var selectionSortedA = selection.Sort(A);
             Console.WriteLine("\nSelection sorted Array: ");
             selectionSortedA.ToList().ForEach(i => Console.Write(i.ToString() + " "));
 
             //Bubble Sort
+            int[] B = { 2, 7, 4, 1, 5, 3 };
+            Console.WriteLine("\n\nActual Array: ");
+            B.ToList().ForEach(i => Console.Write(i.ToString() + " "));
             var bubble = new BubbleSort();
-            var bubbleSortedA = bubble.Sort(A);
+            var bubbleSortedB = bubble.Sort(B);
             Console.WriteLine("\nBubble sorted Array: ");
-            bubbleSortedA.ToList().ForEach(i => Console.Write(i.ToString() + " "));
+            bubbleSortedB.ToList().ForEach(i => Console.Write(i.ToString() + " "));
 
+            //Insertion Sort
+            int[] C = { 2, 7, 4, 1, 5, 3 };
+            Console.WriteLine("\n\nActual Array: ");
+            C.ToList().ForEach(i => Console.Write(i.ToString() + " "));
+            var insertion = new InsertionSort();            
+            var insertionSortedC = insertion.Sort(C);
+            Console.WriteLine("\nInsertion sorted Array: ");
+            insertionSortedC.ToList().ForEach(i => Console.Write(i.ToString() + " "));
             Console.ReadLine();
         }
     }
