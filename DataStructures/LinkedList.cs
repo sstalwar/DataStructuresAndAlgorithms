b4e14d8ec9d1fb2b1330981389a60175c7692805
Sagar Talwar - Intern <Sagar.Talwar@bentley.com>
2020-07-13 18:17:37 -0400

    Added LinkedList

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DataStructures
+{
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+    class LinkedList
+    {
+        private Node head;
+        public LinkedList()
+        {
+            head = null;
+        }
+        public Node AddToBegining(int data)
+        {
+            Node node = new Node(data);
+
+            if (head != null)
+            {
+                node.next = head;
+            }
+
+            head = node;
+            return node;
+        }
+
+
+        public void PrintLinkedList()
+        {
+            Node t = head;
+            if (t == null)
+            {
+                Console.WriteLine("LinkedList is empty");                
+            }
+            else
+            {
+                while (t != null)
+                {
+                    Console.WriteLine(t.data);
+                    t = t.next;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.AddToBegining(1);
+            linkedList.AddToBegining(2);
+            linkedList.AddToBegining(3);
+            linkedList.AddToBegining(4);
+            linkedList.AddToBegining(5);
+            linkedList.PrintLinkedList();
+        }
+    }
+}
