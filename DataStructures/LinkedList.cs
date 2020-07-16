c9728540ebe91ea7080454cb6b692fd3807fc760
sstalwar <sagar.talwar88@gmail.com>
2020-07-15 21:13:31 -0400

    Linkedin Changes

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -27,33 +27,91 @@ namespace DataStructures
         public Node AddToBegining(int data)
         {
             Node node = new Node(data);
-
-            if (head != null)
-            {
-                node.next = head;
-            }
-
+            node.next = head;
             head = node;
             return node;
+        }
+
+        public Node AddToEnd(int data)
+        {
+            var node = new Node(data);
+
+            if (head == null)
+            {
+                head = node;
+                return node;
+            }
+
+            var temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = node;
+            
+            return node;
+        }
+
+        public Node AddToNthPosition(int data, int position)
+        {
+            if (position == 0 || (head == null & position > 1))
+            {
+                Console.WriteLine("\n Error: Please provide correct position");
+                return null;
+            }
+            if (position == 1)
+            {
+                var node = AddToBegining(data);
+                return node;                
+            }
+
+            var temp = head;
+            int i = 1;
+            for (; i < position-1; i++)
+            {
+                if (temp.next == null)
+                {
+                    break;
+                }
+                else
+                {
+                    temp = temp.next;
+                }
+            }
+
+            if (temp.next == null && (i < position - 1))
+            {
+                Console.WriteLine("\n Error: Please provide correct position");
+                return null;
+            }
+            else if (i == position - 1)
+            {
+                var node = new Node(data);
+                node.next = temp.next;
+                temp.next = node;
+                return node;
+            }
+
+            return null;
         }
 
 
         public void PrintLinkedList()
         {
-            Node t = head;
-            if (t == null)
+            Node temp = head;
+            if (temp == null)
             {
                 Console.WriteLine("LinkedList is empty");                
             }
             else
             {
-                while (t != null)
+                Console.WriteLine("\nLinked List:");
+                while (temp != null)
                 {
-                    Console.WriteLine(t.data);
-                    t = t.next;
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
                 }
             }
-            Console.ReadLine();
         }
     }
 }
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,12 +7,77 @@ namespace DataStructures
         static void Main(string[] args)
         {
             LinkedList linkedList = new LinkedList();
-            linkedList.AddToBegining(1);
-            linkedList.AddToBegining(2);
-            linkedList.AddToBegining(3);
-            linkedList.AddToBegining(4);
-            linkedList.AddToBegining(5);
-            linkedList.PrintLinkedList();
+
+            var yes = 'y';
+            while (yes != 'n' && yes !='N')
+            {
+                Console.WriteLine("\nplease select one of the options below:");
+                Console.WriteLine("1. Add element to the beginning: ");
+                Console.WriteLine("2. Add element to the end: ");
+                Console.WriteLine("3. Add element at nth position:");
+                Console.WriteLine("Please enter you choice: ");
+                var choice = Int32.Parse(Console.ReadLine());
+                string input;
+                int data;
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("please enter data to be added to the beginning of Linked List:");
+                        input = Console.ReadLine();
+                        if (Int32.TryParse(input, out data))
+                        {
+                            linkedList.AddToBegining(data);
+                            linkedList.PrintLinkedList();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter numerical data only");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("please enter data to be added to the end of Linked List:");
+                        input = Console.ReadLine();
+                        if (Int32.TryParse(input, out data))
+                        {
+                            linkedList.AddToEnd(data);
+                            linkedList.PrintLinkedList();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter numerical data only");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("please enter data to be added to the Linked List:");
+                        input = Console.ReadLine();
+                        if (Int32.TryParse(input, out data))
+                        {
+                            Console.WriteLine("please enter location to be added to the Linked List:");
+                            var location = Console.ReadLine();
+                            if (Int32.TryParse(location, out int loc))
+                            {
+                                linkedList.AddToNthPosition(data, loc);
+                                linkedList.PrintLinkedList();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please enter numerical data only");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter numerical data only");
+                        }
+                        break;
+                    default: break;
+
+                }
+                Console.WriteLine("\nDo you want to continue?: ");
+                Console.WriteLine("\nPress n to terminate or y to continue: ");
+                yes = (char)Console.ReadKey().Key;
+
+            }
         }
     }
 }
